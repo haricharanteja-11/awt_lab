31c42c8c0a210c00bc2ac1a7d49c2937ea880f23
haricharanteja-11 <haricharanteja6@gmail.com>
2025-02-22 01:43:50 +0530

    Update prototype.js

--- prototype.js
+++ prototype.js
@@ -17,6 +17,6 @@ class Student extends Person {
     }
 }
 
-const student2 = new Student("Varun", "MERN Stack");
+const student2 = new Student("Hari Charan Teja", "MERN Stack");
 student2.greet();  
 student2.study();  
